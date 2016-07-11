5cbc461959e962f6cde43ac1ff129ddbca0c3ab1
Nico <nico.ritsche@gmail.com>
2016-07-11 14:13:45 +0200

    wip

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,40 @@
+defmodule Rumbl.UserController do
+  use Rumbl.Web, :controller
+  plug :authenticate_user when action in [:index, :show]
+
+  alias Rumbl.User
+
+  def index(conn, _params) do
+    page = User
+    |> Repo.paginate(page_size: 30)
+    render conn, "index.html",
+      users: page.entries,
+      page_number: page.page_number,
+      page_size: page.page_size,
+      total_pages: page.total_pages,
+      total_entries: page.total_entries
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(User, id)
+    render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.registration_changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> Rumbl.Auth.login(user)
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+end
